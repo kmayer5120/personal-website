fe1f591e5a520145ae819fcad34900216f707e1c
Kyle Mayer <kmayer1@cnm.edu>
2021-03-20 23:38:42 -0600

    formatting of spaces

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import {Navbar} from "./shared/components/Navbar"
-import {Technologies} from "./shared/components/Technologies"
 import {Home} from "./home/Home"
 
 function App() {
--- frontend/src/shared/components/Technologies.js
+++ frontend/src/shared/components/Technologies.js
@@ -8,11 +8,11 @@ export const Technologies = () => {
         <div className="mx-5">
             <h1 className="text-3xl">Technologies</h1>
             <div className="mx-2 px-3 grid grid-cols-3 grid-rows-3 grid-4">
-                    <FaReact className="technology-icons"/>
-                    <IoLogoNodejs className="technology-icons"/>
-                    <SiTypescript className="technology-icons"/>
-                    <IoLogoHtml5 className="technology-icons"/>
-                    <IoLogoCss3 className="technology-icons"/>
+                <FaReact className="technology-icons"/>
+                <IoLogoNodejs className="technology-icons"/>
+                <SiTypescript className="technology-icons"/>
+                <IoLogoHtml5 className="technology-icons"/>
+                <IoLogoCss3 className="technology-icons"/>
             </div>
         </div>
         </>
