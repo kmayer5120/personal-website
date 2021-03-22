a45574cdd7c9a4243c005486ae13c4fffb06c244
Kyle Mayer <kmayer1@cnm.edu>
2021-03-21 23:19:05 -0600

    added background image and more layout stuff

--- frontend/src/ui/home/Home.js
+++ frontend/src/ui/home/Home.js
@@ -5,12 +5,17 @@ import { AboutMe } from "../shared/components/AboutMe";
 export const Home = () => {
     return (
         <>
-            <body className="box-border bg-white h-screen">
+            <body className="box-border bg-gray-800">
                 <section>
-                    <div className="flex flex-col">
+                    <img
+                        src="https://images.unsplash.com/photo-1584949091598-c31daaaa4aa9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+                        alt="background with code"
+                        className="md:visible invisible bg-center bg-cover min-h-full w-full absolute"
+                    />
+                    <div className="flex flex-col relative">
                         <div className="container mx-auto md:flex">
-                            <AboutMe className="min-w-lg flex-1" />
-                            <Technologies className="flex-1" />
+                            <AboutMe className="flex-grow" />
+                            <Technologies className="flex-grow" />
                         </div>
                     </div>
                 </section>
