3d150a75fb326eb60237e02708c84964e8060ff5
Kyle Mayer <kmayer1@cnm.edu>
2021-03-22 00:04:19 -0600

    added more styling

--- frontend/src/ui/shared/components/AboutMe.js
+++ frontend/src/ui/shared/components/AboutMe.js
@@ -3,48 +3,52 @@ import { Header } from "./Header";
 import "../../../index.css";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import profilePicture from "../../img/kyle-mayer-profile-picture.jpg";
+import "../../../index.css";
 
 export const AboutMe = () => {
     return (
         <>
-            <div className="container bg-indigo-900 border-2 border-white border-opacity-50 rounded-xl shadow-md mt-10 md:mx-5 md:max-w-md">
+            <div className="container glass-blur rounded-xl shadow-md mt-10 md:mx-5 md:max-w-md">
                 <Header label="Bio" />
-                <div className="md:grid grid-cols-1 align-items justify-items-center">
-                    <img
-                        src={profilePicture}
-                        alt="Kyle"
-                        className="my-5 rounded-lg max-w-xs mx-auto"
-                    />
-                    <div className="px-20 flex-row">
-                        <p className="max-w-prose text-indigo-100 my-5">
-                            Hello and welcome to my personal website! My name is
-                            Kyle Mayer.
-                        </p>
-                        <p className="max-w-prose text-indigo-100 my-5">
-                            Passionate and creative problem solver working to
-                            expand my skill set as much as possible every single
-                            day. My previous career as a professional chef
-                            taught me how to be organized, methodical, and a
-                            team player in order to deliver quality products in
-                            a timely manner.
-                        </p>
-                        <div className="grid grid-cols-2 gap-5 mb-5 items-center">
-                            <a
-                                href="https://www.linkedin.com/in/kyle-mayer-fullstack/"
-                                className="text-white hover:text-indigo-300 transition duration-300 ease-in-out text-8xl flex-none"
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                <FaLinkedin />
-                            </a>
-                            <a
-                                href="https://github.com/kmayer5120"
-                                className="text-white hover:text-indigo-300 transition duration-300 ease-in-out text-8xl flex-none"
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                <FaGithub />
-                            </a>
+                <div className="ml-2 my-5 border-l-8 rounded border-yellow-500">
+                    <div className="md:grid grid-cols-1 align-items justify-items-center">
+                        <img
+                            src={profilePicture}
+                            alt="Kyle"
+                            id="img-about-me"
+                            className="my-5 rounded-xl max-w-xs mx-auto shadow-md"
+                        />
+                        <div className="px-20 flex-row">
+                            <p className="max-w-prose text-white my-5">
+                                Hello and welcome to my personal website! My
+                                name is Kyle Mayer.
+                            </p>
+                            <p className="max-w-prose text-white my-5">
+                                Passionate and creative problem solver working
+                                to expand my skill set as much as possible every
+                                single day. My previous career as a professional
+                                chef taught me how to be organized, methodical,
+                                and a team player in order to deliver quality
+                                products in a timely manner.
+                            </p>
+                            <div className="grid grid-cols-2 gap-5 mb-5 items-center">
+                                <a
+                                    href="https://www.linkedin.com/in/kyle-mayer-fullstack/"
+                                    className="text-white hover:text-indigo-300 transition duration-300 ease-in-out text-8xl flex-none"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <FaLinkedin />
+                                </a>
+                                <a
+                                    href="https://github.com/kmayer5120"
+                                    className="text-white hover:text-indigo-300 transition duration-300 ease-in-out text-8xl flex-none"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <FaGithub />
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
--- frontend/src/ui/shared/components/Technologies.js
+++ frontend/src/ui/shared/components/Technologies.js
@@ -17,42 +17,44 @@ import ddcLogo from "../../img/ddc.png";
 export const Technologies = () => {
     return (
         <>
-            <div className="container bg-indigo-900 border-2 border-white border-opacity-50 rounded-xl shadow-md mt-10 md:mx-5 md:max-w-md">
+            <div className="container glass-blur rounded-xl shadow-md mt-10 md:mx-5 md:max-w-md">
                 <Header label="Technologies" />
-                <div className="grid my-5 px-20 grid-flow-cols grid-flow-rows grid-cols-3 grid-rows-3 gap-7">
-                    <FaReact className="technology-icons" />
-                    <SiJavascript className="technology-icons" />
-                    <SiTypescript className="technology-icons" />
-                    <IoLogoHtml5 className="technology-icons" />
-                    <IoLogoCss3 className="technology-icons" />
-                    <FaBootstrap className="technology-icons" />
-                    <SiMysql className="technology-icons" />
-                    <FaNode className="technology-icons" />
-                    <IoLogoDocker className="technology-icons" />
-                </div>
-                <div className="mx-20">
-                    <p className="text-indigo-100 max-w-prose my-5">
-                        I learned these technologies (and many more!) when I
-                        took the Fullstack Deep Dive Coding Bootcamp. I am proud
-                        to be an alumni of this bootcamp.
-                    </p>
-                    <div className="bg-white p-5 my-5">
-                        <a href="https://deepdivecoding.com/">
-                            <img
-                                src={ddcLogo}
-                                alt="Deep Dive Coding Logo"
-                                className="hover:opacity-75 transition ease-in-out duration-300"
-                                target="_blank"
-                                rel="noreferrer"
-                            />
-                        </a>
+                <div className="ml-2 border-l-8 rounded border-indigo-500">
+                    <div className="grid my-5 px-20 grid-flow-cols grid-flow-rows grid-cols-3 grid-rows-3 gap-12">
+                        <FaReact className="technology-icons" />
+                        <SiJavascript className="technology-icons" />
+                        <SiTypescript className="technology-icons" />
+                        <IoLogoHtml5 className="technology-icons" />
+                        <IoLogoCss3 className="technology-icons" />
+                        <FaBootstrap className="technology-icons" />
+                        <SiMysql className="technology-icons" />
+                        <FaNode className="technology-icons" />
+                        <IoLogoDocker className="technology-icons" />
                     </div>
-                    <p className="text-indigo-100 max-w-prose my-5">
-                        This fullstack bootcamp consisted of 400 hours of in
-                        class time. We wrote code daily and participated in a
-                        capstone team project build with MySQL, Express, React,
-                        and Node.
-                    </p>
+                    <div className="mx-20">
+                        <p className="text-white max-w-prose my-5">
+                            I learned these technologies (and many more!) when I
+                            took the Fullstack Deep Dive Coding Bootcamp. I am
+                            proud to be an alumni of this bootcamp.
+                        </p>
+                        <div className="bg-white p-5 my-5 rounded shadow-md">
+                            <a href="https://deepdivecoding.com/">
+                                <img
+                                    src={ddcLogo}
+                                    alt="Deep Dive Coding Logo"
+                                    className="hover:opacity-75 transition ease-in-out duration-300"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                />
+                            </a>
+                        </div>
+                        <p className="text-white max-w-prose my-5">
+                            This fullstack bootcamp consisted of 400 hours of in
+                            class time. We wrote code daily and participated in
+                            a capstone team project build with MySQL, Express,
+                            React, and Node.
+                        </p>
+                    </div>
                 </div>
             </div>
         </>
