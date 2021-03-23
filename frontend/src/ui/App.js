de9ca883f674d934f2eb3f9eaf37e631d94603e7
Kyle Mayer <kmayer1@cnm.edu>
2021-03-23 11:50:01 -0600

    added blogging related routes

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -3,6 +3,8 @@ import { Home } from "./home/Home";
 import { FourOhFour } from "./FourOhFour";
 import { BrowserRouter } from "react-router-dom";
 import { Switch, Route } from "react-router";
+import { Blog } from "./blog/Blog";
+import Post from "./shared/components/sanity/Post";
 
 function App() {
     return (
@@ -10,6 +12,8 @@ function App() {
             <Navbar />
             <BrowserRouter>
                 <Switch>
+                    <Route path="/blog" component={Blog} />
+                    <Route path="/:slug" component={Post} />
                     <Route exact path="/" component={Home} />
                     <Route component={FourOhFour} />
                 </Switch>
