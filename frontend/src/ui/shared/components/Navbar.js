1973ed6a36acfb9961d2262cbba4a9dc7bc139e7
Kyle Mayer <kmayer1@cnm.edu>
2021-03-23 11:50:30 -0600

    modified navbar to have link to blog

--- frontend/src/ui/shared/components/Navbar.js
+++ frontend/src/ui/shared/components/Navbar.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaLinkedin, GiPapers } from "react-icons/all";
 
 export const Navbar = ({ fixed }) => {
+    const linkClasses = "px-2 py-2 flex items-center text-3xl uppercase font-bold leading-snug text-white hover:opacity-75";
     return (
         <>
             <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-gray-900 mb-0">
@@ -18,7 +19,7 @@ export const Navbar = ({ fixed }) => {
                         <ul className="grid grid-flow-col list-none">
                             <li>
                                 <a
-                                    className="px-3 py-2 flex items-center text-3xl uppercase font-bold leading-snug text-white hover:opacity-75"
+                                    className={linkClasses}
                                     href="https://github.com/kmayer5120"
                                 >
                                     <FaGithub />
@@ -26,10 +27,18 @@ export const Navbar = ({ fixed }) => {
                             </li>
                             <li>
                                 <a
-                                    className="px-3 py-2 flex items-center text-3xl uppercase font-bold leading-snug text-white hover:opacity-75"
+                                    className={linkClasses}
                                     href="https://linkedin.com/in/kyle-mayer-fullstack"
                                 >
                                     <FaLinkedin />
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="/blog"
+                                    className={linkClasses}
+                                >
+                                    <GiPapers />
                                 </a>
                             </li>
                         </ul>
