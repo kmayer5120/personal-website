d3b077e782b92bcd0c173f7240cc6dd57db4f604
Kyle Mayer <kmayer1@cnm.edu>
2021-03-24 22:48:36 -0600

    added btn class

---
+++ frontend/src/ui/resume/Resume.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Resume = () => {
+    return (
+        <>
+            <div className="box-border bg-gray-800 pb-10 h-screen mb-auto">
+                <div className="bg-gray-200">
+                    <h2 className="text-2xl">Kyle Mayer</h2>
+                </div>
+            </div>
+        </>
+    )
+}
