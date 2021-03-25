2809a835e0b66575d4a092282449544d5eae4e3c
Kyle Mayer <kmayer1@cnm.edu>
2021-03-24 22:55:42 -0600

    fixed overflow issue

--- frontend/src/ui/home/Home.js
+++ frontend/src/ui/home/Home.js
@@ -5,7 +5,7 @@ import { AboutMe } from "../shared/components/AboutMe";
 export const Home = () => {
     return (
         <>
-            <body className="box-border bg-gray-800 pb-10 h-screen mb-auto">
+            <body className="box-border bg-gray-800 pb-10 h-screen mb-auto overflow-y-scroll">
                 <section className="flex align-items justify-center">
                     <div className="flex flex-col">
                         <div className="container mx-auto md:flex align-items justify-content-center">
