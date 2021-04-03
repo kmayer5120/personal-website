a48ccacbda9d9e98677b9be2572fcbbe51d03d4e
Kyle Mayer <kmayer1@cnm.edu>
2021-04-03 11:33:31 -0600

    adding custom schema for project and project category

---
+++ frontend/sanity-cms/schemas/project.js
@@ -0,0 +1,51 @@
+export default {
+    name: "project",
+    title: "Project",
+    type: "document",
+    fields: [
+        {
+            name: "title",
+            title: "Title",
+            type: "string",
+        },
+        {
+            name: "slug",
+            title: "Slug",
+            type: "slug",
+            options: {
+                source: "title",
+                maxLength: 96,
+            },
+        },
+        {
+            name: "projectCategories",
+            title: "Project Categories",
+            type: "array",
+            of: [{ type: "reference", to: { type: "projectCategory" } }],
+        },
+        {
+            name: "mainImage",
+            title: "Main image",
+            type: "image",
+            options: {
+                hotspot: true,
+            },
+        },
+        {
+            name: "publishedAt",
+            title: "Published at",
+            type: "datetime",
+        },
+        {
+            name: "body",
+            title: "Body",
+            type: "blockContent",
+        },
+    ],
+    preview: {
+        select: {
+            title: "title",
+            media: "mainImage",
+        },
+    },
+};
---
+++ frontend/sanity-cms/schemas/projectCategory.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'projectCategory',
+    title: 'Project Category',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+        },
+        {
+            name: 'description',
+            title: 'Description',
+            type: 'text',
+        },
+    ],
+}
