2a988068c8e66d5579d580d09fa828f63614641a
Kyle Mayer <kmayer1@cnm.edu>
2021-04-05 13:11:48 -0600

    modified project component to have more content

--- frontend/src/ui/shared/components/Project.js
+++ frontend/src/ui/shared/components/Project.js
@@ -1,66 +1,68 @@
-import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import React from "react";
 import sanityClient from "../../../client";
 import { Categories } from "./sanity/Categories";
 import BlockContent from "@sanity/block-content-to-react";
 import imageUrlBuilder from "@sanity/image-url";
+import { Accordion } from "./Accordion";
+import "./css/components.css";
+import { FaGithub, FaLink } from "react-icons/fa";
 
-export const Project = () => {
-    const [project, setProject] = useState(null);
-    const { slug } = useParams();
-
+export const Project = ({ project }) => {
     const builder = imageUrlBuilder(sanityClient);
 
     function urlFor(source) {
         return builder.image(source);
     }
 
-    useEffect(() => {
-        sanityClient
-            .fetch(
-                `*[slug.current == $slug]{
-          title,
-          publishedAt,
-          slug,
-          mainImage{
-            asset->{
-              _id,
-              url
-             }
-           },
-         body,
-         "projectCategories": projectCategories[]->,
-       }`,
-                { slug },
-            )
-            .then((data) => setProject(data[0]))
-            .catch(console.error);
-    }, [slug]);
-
     if (!project) return <div className="text-white">Loading...</div>;
     return (
-        <div className="bg-gray-800 min-h-screen pb-10 text-white md:px-20 sm:px-5">
-            <div className="flex justify-center align-items">
-                <div className="md:flex md:flex-row xs:flex-col items-center align-items gap-1">
-                    <Categories categories={project.projectCategories} />
-                </div>
-            </div>
+        <div className="bg-gray-800 min-w-80 pb-10 text-white md:px-20 sm:px-5">
             <div
-                className=" grid grid-cols-1 grid-auto-rows align-items justify-items-center bg-gray-300 rounded-l text-gray-700">
-                <h2 className="block text-2xl text-indigo-600 mt-5">{project.title}</h2>
+                className="grid grid-cols-1 grid-auto-rows align-items justify-items-center bg-gray-300 rounded-l text-gray-700">
+                <h2 className="block text-4xl text-indigo-600 mt-5">{project.title}</h2>
                 <img
-                    src={urlFor(project.mainImage).width(500).url()}
-                    className="rounded-l mt-5 py-2 md:max-h-96 md:max-h-96 object-cover"
+                    src={urlFor(project.mainImage).width(700).url()}
+                    className="rounded-l mb-5 py-2 md:max-h-96 md:max-h-96 object-cover"
                     alt="project screenshot" />
-                <div className="max-w-prose prose p-5 mb-4 rounded-md shadow-lg">
-                    <BlockContent
-                        blocks={project.body}
-                        projectId={sanityClient.projectId}
-                        dataset={sanityClient.dataset}
-                    />
+                <div className="flex gap-5">
+                    {
+                        project.linkToGitHub &&
+                        <a
+                            href={`${project.linkToGitHub}`}
+                            className="text-indigo-600 hover:text-indigo-500 transition duration-300 ease-in-out text-5xl"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <FaGithub />
+                        </a>
+                    }
+                    {
+                        project.linkToDeployment &&
+                        <a
+                            href={`${project.linkToDeployment}`}
+                            className="text-indigo-600 hover:text-indigo-500 transition duration-300 ease-in-out text-5xl"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <FaLink />
+                        </a>
+                    }
                 </div>
-                <Link to="/portfolio" className="btn btn-secondary my-5">Back to Portfolio</Link>
+                <div className="flex justify-center align-items">
+                    <div className="md:flex md:flex-col xs:flex-col items-center text-center align-items gap-1">
+                        <Categories isPreview={true} categories={project.projectCategories} />
+                    </div>
+                </div>
+                <Accordion className="pb-3">
+                    <div className="max-w-prose prose p-5 mb-4 rounded-md shadow-lg">
+                        <BlockContent
+                            blocks={project.body}
+                            projectId={sanityClient.projectId}
+                            dataset={sanityClient.dataset}
+                        />
+                    </div>
+                </Accordion>
             </div>
         </div>
-    )
-}
+    );
+};
