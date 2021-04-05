4a571e2f289e4fb60130cbfbaff0513ef9dc3583
Kyle Mayer <kmayer1@cnm.edu>
2021-04-05 13:08:33 -0600

    needed to add api version for cms

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -2,6 +2,7 @@ import sanityClient from "@sanity/client";
 
 export default sanityClient({
   projectId: "du8mqcrl", // find this at manage.sanity.io or in your sanity.json
+  apiVersion: '2021-03-25', // use a UTC date string
   dataset: "production", // this is from those question during 'sanity init'
   useCdn: true,
 });
